2b594383f68f650e94331aa584f9170e593d7c86
MZubairLohar <zubairgituser@gmail.com>
2020-07-02 19:32:47 +0500

    change

---
+++ build/precache-manifest.d7e9a2e34316033c8fe7ba6f669bfa01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edb61688f0f358a61cf4f983b6da903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e061af0ae0d1c12d418",
+    "url": "/static/css/main.eb971629.chunk.css"
+  },
+  {
+    "revision": "cccc01f328001ea43331",
+    "url": "/static/js/2.7d3aaaac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d3aaaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e061af0ae0d1c12d418",
+    "url": "/static/js/main.a3445130.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  },
+  {
+    "revision": "5c6d769858f700f60be7d1dc60362747",
+    "url": "/static/media/backgroundmap.5c6d7698.png"
+  }
+]);
